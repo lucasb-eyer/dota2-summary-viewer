92097967fc5bfd741fd8fd5a684461f0716670bc
Vladislav Supalov <V.Supalov@googlemail.com>
2012-12-01 17:12:36 +0100

    static file generation from json data, using jinja2

--- scripts/processDem.py
+++ scripts/processDem.py
@@ -13,6 +13,30 @@ import argparse
 import os
 import os.path as path
 
+import json
+import jinja2
+
+class Renderer():
+    def __init__(self,templateDir, staticDir):
+        self.templateDir = templateDir
+        self.staticDir = staticDir #absolute path to static files
+        loader = jinja2.FileSystemLoader(self.templateDir)
+        self.env = jinja2.Environment(loader=loader)
+
+    def render(self, templateName, data):
+        template = self.getTemplateFile(templateName)
+        return template.render(data=data, staticDir=self.staticDir)
+
+    def getTemplateFile(self, templateName):
+        return self.env.get_template(templateName)
+
+    def renderToAndFromFile(self, outFilePath, templateName, inJsonFilePath):
+        dataFile = open(inJsonFilePath,"r")
+        data = json.loads(dataFile.read())
+
+        outFile = open(outFilePath, "w")
+        outFile.write(self.render(templateName, data))
+
 if __name__ == "__main__":
     p = argparse.ArgumentParser(description="Dota 2 demo processor")
     p.add_argument('demo', help="The .dem file to parse")
@@ -21,37 +45,47 @@ if __name__ == "__main__":
     demoFilePath = args.demo
     demoFile = path.basename(demoFilePath)
     demoFileName, demoFileExt = path.splitext(demoFile)
-    
-    #make sure the output goes to the right place yadda yadda files
-    scriptDir = os.path.realpath(__file__)
-    destinationDirectory = path.normpath(
-        path.join(scriptDir,"..","..","data","output",demoFileName))
 
-    if path.exists(destinationDirectory):
-        if not path.isdir(destinationDirectory):
-            log.error("Destination is not a directory:'%s'"%(destinationDirectory,))
+    #make sure the output goes to the right place yadda yadda files
+    scriptDir = os.path.dirname(os.path.realpath(__file__))
+    destinationDir = path.normpath(
+        path.join(scriptDir,"..","data","output",demoFileName))
+
+    #init renderer
+    templateDir = path.normpath(path.join(scriptDir,"..","templates"))
+    staticDir = path.normpath(path.join(scriptDir,"..","static"))
+    renderer = Renderer(templateDir,staticDir)
+
+    if path.exists(destinationDir):
+        if not path.isdir(destinationDir):
+            log.error("Destination is not a directory:'%s'"%(destinationDir,))
             exit(1)
     else:
-        logging.error("Creating new directory:'%s'"%(destinationDirectory,))
-        os.mkdir(destinationDirectory)
+        logging.error("Creating new directory:'%s'"%(destinationDir,))
+        os.mkdir(destinationDir)
 
     print ("Producing .demson from .dem")
-    demsonPath = path.join(destinationDirectory,"extract.demson")
+    demsonPath = path.join(destinationDir,"extract.demson")
     os.system("demoinfo2 %s > %s"%(
         demoFilePath,demsonPath))
 
     print ("Producing translated combatlog")
-    combatlogPath = path.join(destinationDirectory,"combatlog.demson")
+    combatlogPath = path.join(destinationDir,"combatlog.demson")
     os.system("dota2info_combatlog < %s > %s"%(
         demsonPath,combatlogPath))
 
     print ("Producing faction conflict data")
-    factionConflictPath = path.join(destinationDirectory,"factionConflict.json")
+    factionConflictPath = path.join(destinationDir,"factionConflict.json")
     os.system("dota2info_factionConflict < %s > %s"%(
         demsonPath,factionConflictPath))
 
+    #render a html file from the json data in factionConflictPath
+    outPath = path.join(destinationDir,"viewFactionConflict.html")
+    renderer.renderToAndFromFile(outPath,"factionConflict.html",factionConflictPath)
+
     #TODO: once it actually works
     #call dota2info summary script to process the .demson to the fun stuff - summary.json etc
-    #demoSummaryPath = path.join(destinationDirectory,"summary.json")
+    #demoSummaryPath = path.join(destinationDir,"summary.json")
     #os.system("dota2info_summary --out %s %s"%(
     #    demoSummaryPath, demoDemsonPath))
+
